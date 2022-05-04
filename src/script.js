d4bde197304b0aff7271ccfc7d813b4b6121597d
Egor Dubovik <egorka54896@gmail.com>
2022-05-04 12:47:37 +0300

    init: project 'virtual-keyboard'

---
+++ src/script.js
@@ -0,0 +1,12 @@
+
+// import { Post } from "@models/post";
+// import logo from "./assets/images/webpack-logo";
+// import "./models/babel";
+
+
+import "./style/style.scss";
+
+
+console.log("hello");
+
+
